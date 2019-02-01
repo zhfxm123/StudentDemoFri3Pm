b0f10c7b4712f330259c34712dcb016b970d06db
zhfxm123 <47042343+zhfxm123@users.noreply.github.com>
2019-02-02 05:32:13 +0900

    Update StudentDemo.java

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentDemo.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentDemo.java
@@ -14,6 +14,7 @@ public class StudentDemo
   private String studentID;
   private String name;
   private String program;
+  private String course;
   
   /**
    * A constructor to pass in the given ID and then name
